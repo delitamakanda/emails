e70ae3dce7388130626d7befbefb20d2585caff8
Délita Makanda <delita.makanda@gmail.com>
2025-01-03 22:20:46 +0100

    add prettier and eslint conf

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -49,7 +49,7 @@ const composeEmail = () => {
       </ul>
     </nav>
     <div class="compose-button">
-      <i class="fas fa-plus"></i>
+      <i class="fas fa-plus"/>
       <button @click="composeEmail">Nouveau message</button>
     </div>
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/image', '@pinia/nuxt'],
+  modules: ['@nuxt/image', '@pinia/nuxt', '@nuxt/eslint'],
   image: {
     quality: 80,
     formats: ['avif', 'jpeg', 'png'],
